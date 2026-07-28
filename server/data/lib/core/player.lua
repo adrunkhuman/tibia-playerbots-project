971865f0bc145f805767ed967ca4fe2f21aaa143
adrunkhuman <16039109+adrunkhuman@users.noreply.github.com>
2026-07-29 01:09:36 +0200

    server: restore NPC vocation predicates

--- server/data/lib/core/player.lua
+++ server/data/lib/core/player.lua
@@ -40,6 +40,26 @@ function Player.hasRookgaardShield(self)
 		or self:getItemCount(2530) > 0
 end
 
+function Player.isDruid(self)
+	return isInArray({2, 6}, self:getVocation():getId())
+end
+
+function Player.isKnight(self)
+	return isInArray({4, 8}, self:getVocation():getId())
+end
+
+function Player.isMage(self)
+	return isInArray({1, 2, 5, 6}, self:getVocation():getId())
+end
+
+function Player.isPaladin(self)
+	return isInArray({3, 7}, self:getVocation():getId())
+end
+
+function Player.isSorcerer(self)
+	return isInArray({1, 5}, self:getVocation():getId())
+end
+
 function Player.hasFlag(self, flag)
 	return self:getGroup():hasFlag(flag)
 end
