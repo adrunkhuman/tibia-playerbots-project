a85bcd3437b4325dc26af26acb3f05f4a2610e73
adrunkhuman <16039109+adrunkhuman@users.noreply.github.com>
2026-07-28 17:19:11 +0200

    infra: fix seeded GOD account type

--- server/schema/insertGOD.sql
+++ server/schema/insertGOD.sql
@@ -1,5 +1,5 @@
 INSERT INTO accounts (id, name, password, secret, type, premium_ends_at, email, creation)
-VALUES ('1', 'admin', SHA1('admin'), NULL, '3', '0', '', '0')
+VALUES ('1', 'admin', SHA1('admin'), NULL, '6', '0', '', '0')
 ON DUPLICATE KEY UPDATE id = id;
 
 INSERT INTO players (name, group_id, account_id, level)
